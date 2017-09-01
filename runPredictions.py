17bb9976a0a9614b3c20c82952c986b4d75bba5c
Kshitij G <kshitijgorde@rocketmail.com>
2017-09-01 16:38:18 -0400

    updated dataset and classifiers

--- runPredictions.py
+++ runPredictions.py
@@ -6,12 +6,21 @@ from MyDecisionTreeClassifier import MyClassifiers
 
 dir_name = os.path.dirname(os.path.realpath(__file__))
 
-df = pd.read_pickle('LACityRealData/LACity_cleaned.pickle')
-df_labels = pd.read_pickle('LACityRealData/LACity_labels.pickle')
+df = pd.read_pickle('LACityRealData/train_LACity_cleaned.pickle')
+df_labels = pd.read_pickle('LACityRealData/train_LACity_labels.pickle')
 
 test_df = pd.read_pickle('LACityRealData/test_LACity_cleaned.pickle')
 test_df_labels = pd.read_pickle('LACityRealData/test_LACity_labels.pickle')
 
+print df.columns
+del df[unicode('Total Payments')]
+del test_df[unicode('Total Payments')]
+del df[unicode('Payments Over Base Pay')]
+del test_df[unicode('Payments Over Base Pay')]
+del df[unicode('Base Pay')]
+del test_df[unicode('Base Pay')]
+#test_df.drop(unicode('Total Payments'))
+print len(df.columns)
 # Test - 3750
 # Train - 15000
 clf = MyClassifiers()
@@ -24,8 +33,12 @@ df_labels = pd.np.array(df_labels)
 test_df = pd.np.array(test_df)
 test_df_labels = pd.np.array(test_df_labels)
 clf.classify(df,df_labels,test_df,test_df_labels)
-
-
-
-stringTemplate = 'pos1={1} pos2={2} pos3={3} foo={4} bar={5}'
-#print formatString(stringTemplate, 'test','a','b','c')
+#
+# counter=0
+# for item in test_df:
+#     counter+=1
+#     print counter
+#     for item2 in df:
+#         if np.sum(item - item2)==0:
+#             print item,item2
+#
